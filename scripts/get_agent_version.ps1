e7d4e7a2fc77753d6354e19aa0b465a2c956c51a
Eric van Wijk <eric@van-wijk.com>
2022-12-12 17:00:41 +0100

    Fix v3 macOS M1; Increase logging

--- scripts/get_agent_version.ps1
+++ scripts/get_agent_version.ps1
@@ -15,14 +15,13 @@ param (
     [parameter(Mandatory=$false)]
     [ValidateSet(2, 3)]
     [int]
-    $MajorVersion=3
+    $MajorVersion=2
 ) 
 
 (Invoke-RestMethod -Uri https://api.github.com/repos/microsoft/azure-pipelines-agent/releases) | Where-Object {!$_.draft} `
-                                                                                               | Sort-Object -Property @{Expression = "created_at"; Descending = $true} `
                                                                                                | Where-Object {$_.name -match "^v${MajorVersion}"} `
+                                                                                               | Sort-Object -Property @{Expression = "created_at"; Descending = $true} `
                                                                                                | Set-Variable releases
-
 switch ($Channel) {
     "Stable" {
         $releases | Where-Object {!$_.prerelease} `
@@ -47,16 +46,15 @@ if (!$release) {
     Write-Warning "No agent release found for v${MajorVersion}, '${Channel}' channel"
     exit
 }
-
+Write-Debug "release: ${release}"
 $release.name -replace "^v","" | Set-Variable agentVersion
-
 
 if ($IsWindows) {
     $osString = [Environment]::Is64BitProcess ? "win-x64" : "win-x86"
     $extension = "zip"
 }
 if ($IsMacOS) {
-    $osString = ($PSVersionTable.OS -imatch "ARM64") ? "osx-arm64" : "osx-x64"
+    $osString = (($PSVersionTable.OS -imatch "ARM64") -and $MajorVersion -ge 3) ? "osx-arm64" : "osx-x64"
     $extension = "tar.gz"
 }
 if ($IsLinux) {
@@ -77,7 +75,11 @@ $packagePrefix = $ExcludeNode6 ? "pipelines" : "vsts"
 "{0}-agent-{1}-{2}.{3}" -f $packagePrefix, $osString, $agentVersion, $extension | Set-Variable agentPackage
 "https://vstsagentpackage.azureedge.net/agent/{0}/{1}" -f $agentVersion, $agentPackage | Set-Variable packageUrl
 
-Write-Verbose "Validating whether package exists at '${packageUrl}'..."
-Invoke-WebRequest -method HEAD $packageUrl | Out-Null
-
-Write-Host "Agent package url: ${packageUrl}"
+try {
+    Write-Verbose "Validating whether package exists at '${packageUrl}'..."
+    Invoke-WebRequest -method HEAD $packageUrl | Set-Variable packageResponse
+    $packageResponse | Format-List | Out-String | Write-Debug
+    Write-Host "Agent package exists at url: ${packageUrl}"
+} catch {
+    Write-Warning "Could not access agent package url: ${packageUrl}. $($_.Exception.Message)"
+}
