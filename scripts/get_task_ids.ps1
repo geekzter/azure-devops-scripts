f0708fa87b1ea88c407ef34c6922502f1367cc2f
Eric van Wijk <eric@van-wijk.com>
2023-05-17 16:51:49 +0200

    Filter deprecated tasks

--- scripts/get_task_ids.ps1
+++ scripts/get_task_ids.ps1
@@ -1,7 +1,7 @@
 #!/usr/bin/env pwsh
 <# 
 .SYNOPSIS 
-    Get latest version of agent
+    Get task ID's
 #> 
 #Requires -Version 7.2
 param ( 
@@ -11,26 +11,43 @@ param (
 
     [parameter(Mandatory=$false)]
     [switch]
-    $TaskIds=$false    
+    $DeprecatedTasksOnly,
+
+    [parameter(Mandatory=$false)]
+    [switch]
+    $ShowTaskIdsOnly    
 ) 
 
 Get-ChildItem -Path (Join-Path $RepoDirectory task.json) -Recurse -Force -ErrorAction SilentlyContinue | Select-Object -ExpandProperty FullName
                                                                                                        | Set-Variable taskJsonLocations
-                                                                                                       
+
+if (!$taskJsonLocations) {
+    Write-Error "No task.json files found in ${RepoDirectory}, specify -RepoDirectory"
+    exit 1
+}
+                                                     
 [System.Collections.ArrayList]$tasks = @()
 foreach ($taskJson in $taskJsonLocations) {
     Write-Debug $taskJson
     Get-Content $taskJson | ConvertFrom-Json -AsHashtable | Set-Variable task
+    $task | Format-Table | Out-String | Write-Debug
     $tasks.Add($task) | Out-Null
 }
 
-if ($TaskIds) {
+$tasks | ForEach-Object {[PSCustomObject]$_} `
+       | Where-Object {!$DeprecatedTasksOnly -or $_.deprecated} `
+       | ForEach-Object {
+    "{0}@{1}" -f $_.name, $_.version.Major | Set-Variable fullName
+    $_ | Add-Member -MemberType NoteProperty -Name fullName -Value $fullName
+    $_
+} | Set-Variable tasks
+
+if ($ShowTaskIdsOnly) {
     $tasks | Select-Object -ExpandProperty id | Get-Unique
 } else {
-    # $tasks[0]
-    $tasks | ForEach-Object {[PSCustomObject]$_} `
-           | Format-Table -Property name, `
+    $tasks | Format-Table -Property fullName, `
                                     id, `
+                                    deprecated, `
                                     friendlyName, `
                                     description, `
                                     version, `
