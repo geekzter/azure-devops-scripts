5d18209ba7dfd350682ec60a901bad19c9558c1a
Eric van Wijk <eric@van-wijk.com>
2023-06-25 13:39:45 +0200

    Refactor list_tasks.ps1

--- scripts/list_tasks.ps1
+++ scripts/list_tasks.ps1
@@ -1,13 +1,13 @@
 #!/usr/bin/env pwsh
 <# 
 .SYNOPSIS 
-    Get task ID's
+    Get Azure Pipeline task data from the Azure Pipelines task repo
 #> 
 #Requires -Version 7.2
 param ( 
-    [parameter(Mandatory=$false)][string]
-    [ValidateNotNull()]
-    $RepoDirectory=(Get-Location).Path,
+    [parameter(Mandatory=$false)]
+    [string]
+    $RepoDirectory,
 
     [parameter(Mandatory=$false)]
     [switch]
@@ -18,9 +18,28 @@ param (
     $DeprecatedTasksOnly,
 
     [parameter(Mandatory=$false)]
-    [switch]
-    $ShowTaskIdsOnly    
+    [string]
+    [ValidateSet("Csv","Ids","Table")]
+    $Format="Table",
+
+    [parameter(Mandatory=$false)]
+    [string[]]
+    #$Property=@("id","name","friendlyName","author","helpUrl","category","visibility","runsOn","version","preview","instanceNameFormat","groups","inputs","dataSourceBindings","execution","fullName","majorVersion")
+    $Property=@("fullName","id","name","friendlyName","version","majorVersion")
 ) 
+
+if (!$RepoDirectory) {
+    # Try to find task repo directory
+    $directoryElements = $PSScriptRoot.Split([IO.Path]::DirectorySeparatorChar)
+    $directoryElements[0..($directoryElements.Length-4)] -join [IO.Path]::DirectorySeparatorChar `
+                                                         | Set-Variable RepoBaseDirectory
+
+    Join-Path $RepoBaseDirectory "microsoft" "azure-pipelines-tasks" | Set-Variable RepoDirectory
+    if (!(Test-Path $RepoDirectory)) {
+        Write-Warning "No RepoDirectory specifed"
+        exit
+    }
+}
 
 Get-ChildItem -Path (Join-Path $RepoDirectory task.json) -Recurse -Force -ErrorAction SilentlyContinue | Select-Object -ExpandProperty FullName
                                                                                                        | Set-Variable taskJsonLocations
@@ -38,26 +57,38 @@ foreach ($taskJson in $taskJsonLocations) {
     $tasks.Add($task) | Out-Null
 }
 
+# Filter tasks
 $tasks | ForEach-Object {[PSCustomObject]$_} `
        | Where-Object {!$AzureTasksOnly -or ($_ | Select-Object -ExpandProperty inputs -ErrorAction SilentlyContinue | Where-Object -Property type -ieq 'connectedService:AzureRM')} `
        | Where-Object {!$DeprecatedTasksOnly -or $_.deprecated} `
-       | ForEach-Object {
-            "{0}@{1}" -f $_.name, $_.version.Major | Set-Variable fullName
-            $_ | Add-Member -MemberType NoteProperty -Name fullName -Value $fullName
-            $_
-       } `
-       | Sort-Object -Property fullName `
        | Set-Variable tasks
 
-if ($ShowTaskIdsOnly) {
-    $tasks | Select-Object -ExpandProperty id | Get-Unique
+# Format results
+if ($Format -eq "Ids") {
+    $tasks | Select-Object -ExpandProperty id `
+           | Get-Unique `
+           | Sort-Object `
+           | Set-Variable tasks
 } else {
-    $tasks | Format-Table -Property fullName, `
-                                    id, `
-                                    deprecated, `
-                                    friendlyName, `
-                                    description, `
-                                    version, `
-                                    @{Label="version"; Expression={$_.version.ToString()}}, `
-                                    author
+    $tasks | ForEach-Object {
+                "{0}@{1}" -f $_.name, $_.version.Major | Set-Variable fullName
+                $_ | Add-Member -MemberType NoteProperty -Name fullName -Value $fullName
+                $_ | Add-Member -MemberType NoteProperty -Name majorVersion -Value $_.version.Major
+                $_.version = ("{0}.{1}.{2}" -f $_.version.Major, $_.version.Minor, $_.version.Patch)
+                $_
+           } `
+           | Sort-Object -Property fullName `
+           | Select-Object -Property $Property `
+           | Set-Variable tasks
+}
+
+# Display results
+$tasks | Format-Table
+
+# Export results
+if ($Format -eq "Csv") {
+    $csvFullName = "$((New-TemporaryFile).FullName).csv"
+    $tasks | Select-Object -ExcludeProperty description, helpMarkDown, releaseNotes `
+           | Export-Csv -Path $csvFullName
+    Write-Host "Results exported to ${csvFullName}"
 }
